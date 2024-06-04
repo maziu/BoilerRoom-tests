0031f8dad13b6017761c938664757f708816b58e
Mateusz Juzwiak <mazicort@gmail.com>
2024-06-04 15:12:39 +0200

    First smoke test OK

--- modbus_server.py
+++ modbus_server.py
@@ -1,33 +1,130 @@
 
 import sys
-from enum import Enum
+from enum import IntEnum
 
 import modbus_tk
 import modbus_tk.defines as cst
 from modbus_tk import modbus_tcp
 from modbus_tk import modbus_rtu
 
+from threading import Thread, Lock, Event
+from  time import sleep
 
-class Input(Enum):
+class Input(IntEnum):
     EXAMPLE_INPUT = 1
     EXAMPLE_IN2 = 2
 
-class TempSensor(Enum):
-    TS_3D = 100
-    TS_WEATHER = 101
-    TS_XXX = 2
+class HMIRead(IntEnum):
+    REG_DeviceStatus = 200
+    REG_IO_Inputs = 201
+    REG_3D_Setpoint = 202
+    REG_CyclesPerSec = 203
+    REG_ParamsTimestamp = 204
+    REG_Temperatures = 205
+    REG_TS_3D = 205
+    REG_TS_WEATHER = 206
+    REG_TS_BUF = 207
+    REG_TS_CWU = 208
+    REG_TS_BOILER = 209
+    REG_TS_INT = 210
+    REG_TS_RESERVED = 211
+    REG_OpMode = 212
 
-class Output(Enum):
-    OUT_integrator = 0
-    OUT_XXX = 1   
+class OPMODE(IntEnum):
+    OPMODE_Off = 0
+    OPMODE_Preheat = 1
+    OPMODE_Work = 2
+    OPMODE_Final = 3
 
-class Param(Enum):
-    P_abc = 0
-    P_cde = 1
-    P_qwe = 2
+class HMIWrite(IntEnum):
+    REG_MODE = 100
+    REG_CMD = 101
+    REG_FLAGS = 102
+    REG_TestMode = 103
+    REG_ParamsTimestamp = 104
+    REG_PARAMS = 105
+    REG_SIM_TEMP = 144
+
+class TestMode(IntEnum):
+    REG_TestMode_Off = 0
+    REG_TestMode_1 = 0xFEED
+
+class TempSensor(IntEnum):
+    TS_3D = 0
+    TS_WEATHER = 1
+    TS_BUF = 2
+    TS_CWU = 3
+    TS_BOILER = 4
+    TS_INT = 5
+
+class Flags(IntEnum):
+    FLAG_SaveParams = 0
+    FLAG_StartBoiler = 1
+    FLAG_SimTemp = 2
+    FLAG_FanKeepalive = 3
+
+class Devices_OUT(IntEnum):
+    DEVO_PCirc = 0
+    DEVO_Belimo2 = 1
+    DEVO_Belimo1 = 2
+    DEVO_PBuf = 3
+    DEVO_VI2_CWU = 4
+    DEVO_VI2_Heater = 5
+    DEVO_Lighter = 6
+    DEVO_Fan = 7   
+    DEVO_PBoiler = 8
+    DEVO_VI1_Heater = 9
+    DEVO_PHeater = 10
+    DEVO_VI1_CWU = 11
+    DEVO_V3D_Close = 12
+    DEVO_V3D_Open = 13     
+
+class Devices_IN(IntEnum):
+    DEVI_PFireplace = 0
+    DEVI_BtnCirc = 1
+    DEVI_BtnLighter = 2
+    DEVI_SigThermostat = 3
+
+
+class Param(IntEnum):
+    P_IGN_TIME_105 = 0
+    P_FAN_TIME_MIN_106 = 1
+    P_FAN_TEMP_MAX_107 = 2
+    P_FAN_TEMP_MIN_108 = 3
+    P_FAN_TIME_KEEP_109 = 4
+    #5 110 reserved
+    P_PUMP_TEMP_ON_111 = 6
+    P_PUMP_TEMP_OFF_112 = 7
+
+class Temp:
+    def __init__(self, i):
+        if(type(i) == type(1.23)):
+            self.float = i
+            self.int = self.float_to_bus(i)
+        elif(type(i) == type(123)):
+            self.int = i
+            self.float = self.bus_to_float(i)
+        else:
+            raise ValueError("Incorrect temperature type specified!")
+
+    def __int__(self):
+        return self.int
+    
+    def __float__(self):
+        return self.float
+
+    def bus_to_float(self, bus):
+        x : float = float(bus)
+        x = x / 10.0
+        return x
+
+    def float_to_bus(self, float):
+        x : int = int((float * 10))
+        return x
 
 class Modbus_ControllableServer():
 
+    BL_NAME = "BL_1"
 
     def __init__(self, port, serverType = "Serial", slaveID = 5):
         self.logger = modbus_tk.utils.create_logger(name="console", record_format="%(message)s")
@@ -47,13 +144,60 @@ class Modbus_ControllableServer():
         self.logger.info("Server initialized")
 
         self.slave = self.server.add_slave(self.slaveID)
-        self.slave.add_block('0', cst.HOLDING_REGISTERS, 100, 300)
+        self.slave.add_block(self.BL_NAME, cst.HOLDING_REGISTERS, 100, 300)
+
+        self.dataLock = Lock()
+
+    def setRegister(self, addr, value):
+        self.slave.set_values(self.BL_NAME, addr, value)
+
+    def getRegister(self, addr) -> int:
+        values = self.slave.get_values(self.BL_NAME, addr, 1)
+        return values[0]
+
+    def setBitInRegister(self, addr, bitNumber):
+        pass
+    
+    def clrBitInRegister(self, addr, bitNumber):
+        pass
+
+    def getBitInRegister(self, addr, bitNumber) -> bool:
+        pass
+
+#--- Application - related functions starts here
+
+    def enableTestMode(self):
+        with self.dataLock:
+            self.setRegister(HMIWrite.REG_TestMode, TestMode.REG_TestMode_1)
+            self.setBitInRegister(int(HMIWrite.REG_FLAGS), Flags.FLAG_SimTemp)
+            sleep(2) # Wait to be sure that value is updated in PLC
+
+    def disableTestMode(self):
+        with self.dataLock:
+            self.setRegister(HMIWrite.REG_TestMode, TestMode.REG_TestMode_Off)
+            self.clrBitInRegister(HMIWrite.REG_FLAGS, Flags.FLAG_SimTemp)
+        sleep(2) # Wait to be sure that value is updated in PLC
 
     def stop_server(self):
         self.server.stop()
 
+    def waitForUpdate(self):
+        sleep(1.5)
+
     def setTemperature(self, sensor : TempSensor, value : float):
-        pass
+        if type(value) != type(12.3):
+            raise ValueError("Invalid temp type, should be float. Please use 12.3 format instead of just 12")
+        value = Temp(value)
+        if sensor < TempSensor.TS_3D or sensor > TempSensor.TS_INT:
+            raise ValueError("Invalid temp sensor ID")
+        with self.dataLock:
+            self.setRegister(HMIWrite.REG_SIM_TEMP + sensor, int(value))
+
+    def getTemperature(self, sensor : TempSensor) -> float:
+        if sensor < TempSensor.TS_3D or sensor > TempSensor.TS_INT:
+            raise ValueError("Invalid temp sensor ID")
+        t = Temp(self.getRegister(HMIRead.REG_Temperatures + sensor))
+        return float(t)
 
     def setInput(self, input : Input):
         pass
@@ -64,8 +208,8 @@ class Modbus_ControllableServer():
     def saveParams(self): #Set value, clear after 2-3s
         pass
 
-    def getOutput(self, output : Output) -> bool:
-        pass
+    #def getOutput(self, output : Output) -> bool:
+    #    pass
 
     def getCounters(self) -> dict:
         pass 
--- test_main.py
+++ test_main.py
@@ -1,8 +1,10 @@
 
-from modbus_server import Modbus_ControllableServer
+from modbus_server import Modbus_ControllableServer, Temp
+import modbus_server as t
 
 import pytest
 import logging
+import random
 
 class TestClass:
     
@@ -15,21 +17,36 @@ class TestClass:
         cls.xVar = 3
         cls.log.info("Setting up test env...")
         cls.server = Modbus_ControllableServer(1001, "TCP")
+        cls.server.enableTestMode()
         cls.log.info("Done!")
 
     @classmethod
     def teardown_class(cls):
         cls.log.info("Finalizing tests...")
+        cls.server.disableTestMode()
         cls.server.stop_server()
         cls.log.info("Done!")
-        
+
     @pytest.mark.order(1)
     def test_smoke(self):
-        self.log.info("Smoke test: PLC connection test")
+        self.log.info("Smoke test: Temperature types")
+        t1 = Temp(123)
+        assert (float(t1)) == 12.3
+        assert (int(t1)) == 123
+        t2 = Temp(15.4)
+        assert (float(t2)) == 15.4
+        assert (int(t2)) == 154
         assert (1+1) == 2
+        self.log.info("Smoke test: Connection to PLC enable test mode and check temp setting")
+        r = float(random.randint(0, 500))/10
+        self.server.setTemperature(t.TempSensor.TS_3D, r)
+        self.server.waitForUpdate()
+        assert (self.server.getTemperature(t.TempSensor.TS_3D)) == r,  \
+                f"Basic get / set temperature test failed, no PLC connection or invalid PLC program"
 
     def test_base_2(self):
-        assert (self.xVar) == 3
+        
+        assert (1+2) == 3
 
 
 if __name__ == "__main__":
